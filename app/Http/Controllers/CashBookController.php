b2b63886cb4e03640b8c609e4b78e7036ab96a13
Vishnupriya <vishnupriyapp08@gmail.com>
2025-01-06 14:11:21 +0530

    cash book successfully added

--- app/Http/Controllers/CashBookController.php
+++ app/Http/Controllers/CashBookController.php
@@ -16,31 +16,38 @@ class CashBookController extends Controller
         $title = 'Cashbook';
         $page = 'cash book';
 
-        // Fetching unique partner first names
-        $partnerNames = DB::table('partners')
-            ->join('sales_partners', 'partners.id', '=', 'sales_partners.partner_id')
-            ->join('sales', 'sales_partners.sale_id', '=', 'sales.id')
-            ->select('partners.first_name')
+        // Get the unique partners (based on partner_cash_installments table)
+        $uniquePartners = DB::table('cash_installments')
+            ->join('sales', 'cash_installments.sale_id', '=', 'sales.id')
+            ->join('cashinstallment_payments', 'cash_installments.id', '=', 'cashinstallment_payments.cash_installment_id')
+            ->join('partner_cash_installments', 'cashinstallment_payments.id', '=', 'partner_cash_installments.cashinstallment_payment_id')
+            ->join('partners', 'partner_cash_installments.partner_id', '=', 'partners.id')
             ->distinct()
-            ->get();
+            ->select('partners.id', 'partners.first_name')
+            ->get();  // Fetch unique partners
 
-        $installments = DB::table('installments')
-            ->join('sales', 'installments.sale_id', '=', 'sales.id') // Join with sales table
-            ->join('sales_partners', 'sales.id', '=', 'sales_partners.sale_id') // Correct sales to sales_partners join
-            ->join('partners', 'sales_partners.partner_id', '=', 'partners.id') // Now join with partners table via sales_partners
+
+        // Fetch cash installments along with necessary information
+        $cashInstallments = DB::table('partner_cash_installments')
+            ->join('partners', 'partner_cash_installments.partner_id', '=', 'partners.id')
+            ->join('cashinstallment_payments', 'partner_cash_installments.cashinstallment_payment_id', '=', 'cashinstallment_payments.id')
+            ->join('cash_installments', 'cashinstallment_payments.cash_installment_id', '=', 'cash_installments.id')
+            ->join('sales', 'cash_installments.sale_id', '=', 'sales.id')
             ->select(
-                'installments.installment_date',
-                'installments.installment_number',
-                'installments.payment_date',
-                'installments.paid_amount',
-                'installments.sale_id',
-                'sales.customer_name',
-                'sales.partner_amounts',
-                'partners.first_name',
-                'sales_partners.percentage',
-                'sales_partners.partner_id'
+                'partner_cash_installments.cashinstallment_payment_id',
+                'partner_cash_installments.partner_id',
+                'partner_cash_installments.amount',
+                'partner_cash_installments.percentage',
+                'partners.first_name',  // Fetch first_name from the partners table
+                'cashinstallment_payments.paid_amount',
+                'cashinstallment_payments.payment_date',
+                'cash_installments.installment_date',
+                'cash_installments.installment_number',
+                'cash_installments.sale_id',
+                'sales.customer_name'  // Fetch customer_name from the sales table
             )
-            ->get();
+            ->get();  // Fetch the installments data
+
 
 
         // Return view with the data
@@ -48,9 +55,10 @@ class CashBookController extends Controller
             'building',
             'title',
             'page',
-            'installments',
-            'partnerNames'
+            'cashInstallments',
+            'uniquePartners'
         ));
+
     }
 
 
@@ -92,7 +100,7 @@ class CashBookController extends Controller
 
 
         // Passing both partner and installments data to the view
-        return view('cashbook.partner_bank', compact('partner', 'installments', 'building', 'title', 'page','partnerName'));
+        return view('cashbook.partner_bank', compact('partner', 'installments', 'building', 'title', 'page', 'partnerName'));
 
 
     }
@@ -129,20 +137,21 @@ class CashBookController extends Controller
             ->get();
 
 
-            $pdf = PDF::loadView('pdf.cash_account_report_pdf', compact(
-                'building',
+        $pdf = PDF::loadView('pdf.cash_account_report_pdf', compact(
+            'building',
             'installments',
             'partnerNames', // Pass the list of bank names to the view
-        
-           
-            ));
-    
-            return $pdf->download('cash_account_report.pdf');
+
+
+        ));
+
+        return $pdf->download('cash_account_report.pdf');
 
 
     }
 
-    public function cashpartnerPDF($buildingId){
+    public function cashpartnerPDF($buildingId)
+    {
 
         $building = Building::findOrFail($buildingId);
 
@@ -177,14 +186,14 @@ class CashBookController extends Controller
 
 
 
-             $pdf = PDF::loadView('pdf.partner_bank_report_pdf', compact(
-                'building',
+        $pdf = PDF::loadView('pdf.partner_bank_report_pdf', compact(
+            'building',
             'installments',
-           'partnerName',
-           'partner'
-            ));
-    
-            return $pdf->download('partner_bank_report.pdf');
+            'partnerName',
+            'partner'
+        ));
+
+        return $pdf->download('partner_bank_report.pdf');
 
 
     }
